793a01ac7b1b7f34ac33b279a946a737484b4855
timand1 <80768929+timand1@users.noreply.github.com>
2023-02-10 18:55:59 +0100

    fix text error

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <div className='about' id='about'>
         <h1>About me</h1>
-        <p>I am a 32 year man old living in Karlstad, Sweden with my SO and daughter.
+        <p>I am a 32 year old man living in Karlstad, Sweden with my SO and daughter.
             Previously educated as a pharmacist at Karlstad University but wanted to do something that I'm excited about.
             Therefore I joined a frontend program to learn JavaScript, CSS and HTML as few examples.
             I am very eager to put my new skillset in use and start this new chapter of my life.</p>
